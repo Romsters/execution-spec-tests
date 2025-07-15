0260b90d113e2d086ce809bb595c90d56ea76187
Anton Baliasnikov <aba@matterlabs.dev>
2025-07-15 15:36:08 +0100

    chore: fix test_coverage

--- tests/homestead/coverage/test_coverage.py
+++ tests/homestead/coverage/test_coverage.py
@@ -63,8 +63,8 @@ def test_coverage(
             value=0,
             protected=False,
             access_list=[],
-            max_fee_per_gas=10,
-            max_priority_fee_per_gas=5,
+            # max_fee_per_gas=10,
+            # max_priority_fee_per_gas=5,
         )
     else:
         tx = Transaction(
